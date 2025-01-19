332a80525655fe3b840d1a70cf7436f683c2021d
Aris <frestaris@gmail.com>
2025-01-19 22:56:34 +1000

    added submenus on navbar links on small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,15 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isSubMenuAboutUsOpen, setIsSubMenuAboutUsOpen] = useState(false);
+  const [isSubMenuOurTeamOpen, setIsSubMenuOurTeamOpen] = useState(false);
 
   const activeClassName = "text-primary font-semibold";
 
   const closeMenu = () => {
     setIsOpen(false);
+    setIsSubMenuAboutUsOpen(false);
+    setIsSubMenuOurTeamOpen(false);
   };
 
   return (
@@ -39,57 +43,55 @@ const Navbar = () => {
               services
             </NavLink>
           </li>
-          <li>
-            <div className="relative group">
-              <NavLink
-                to="/our-team"
-                className={({ isActive }) =>
-                  isActive
-                    ? activeClassName
-                    : "hover:text-primary transition duration-200"
-                }
-              >
-                our team
-              </NavLink>
-              {/* Dropdown Content */}
-              <div className="absolute left-0 hidden group-hover:block bg-gray-100 shadow-lg p-4 w-40">
-                <ul>
-                  <li>
-                    <NavLink
-                      to="/our-team"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      overview
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/dentist-dr-john-doe"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      dr. john doe
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/dentist-dr-jane-smith"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      dr. jane smith
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/dentist-dr-alice-johnson"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      dr. alice johnson
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
+          <div className="relative group">
+            <NavLink
+              to="/our-team"
+              className={({ isActive }) =>
+                isActive
+                  ? activeClassName
+                  : "hover:text-primary transition duration-200"
+              }
+            >
+              our team
+            </NavLink>
+            {/* Dropdown Content */}
+            <div className="absolute left-0 hidden group-hover:block bg-gray-100 shadow-lg p-4 w-40">
+              <ul>
+                <li>
+                  <NavLink
+                    to="/our-team"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    overview
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/dentist-dr-john-doe"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    dr. john doe
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/dentist-dr-jane-smith"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    dr. jane smith
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/dentist-dr-alice-johnson"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    dr. alice johnson
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-          </li>
+          </div>
         </ul>
 
         {/* Logo */}
@@ -97,7 +99,7 @@ const Navbar = () => {
           Dental <span className="text-primary">Center</span>
         </div>
 
-        {/* Right Navigation Links  */}
+        {/* Right Navigation Links */}
         <ul className="hidden md:flex space-x-6 text-gray-600 font-medium">
           <li>
             <NavLink
@@ -111,58 +113,55 @@ const Navbar = () => {
               contact us
             </NavLink>
           </li>
-          <li>
-            <div className="relative group">
-              <NavLink
-                to="/about"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-primary font-bold"
-                    : "hover:text-primary transition duration-200"
-                }
-              >
-                about us
-              </NavLink>
-
-              {/* Dropdown Content */}
-              <div className="absolute left-0 hidden group-hover:block bg-gray-100 shadow-lg p-4 w-36">
-                <ul>
-                  <li>
-                    <NavLink
-                      to="/office"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      office tour
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/reviews"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      reviews
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/safety"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      safety
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/faq"
-                      className="block p-1 text-gray-700 hover:text-primary  text-sm"
-                    >
-                      faq
-                    </NavLink>
-                  </li>
-                </ul>
-              </div>
+          <div className="relative group">
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-primary font-bold"
+                  : "hover:text-primary transition duration-200"
+              }
+            >
+              about us
+            </NavLink>
+            {/* Dropdown Content */}
+            <div className="absolute left-0 hidden group-hover:block bg-gray-100 shadow-lg p-4 w-36">
+              <ul>
+                <li>
+                  <NavLink
+                    to="/office"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    office tour
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/reviews"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    reviews
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/safety"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    safety
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink
+                    to="/faq"
+                    className="block p-1 text-gray-700 hover:text-primary  text-sm"
+                  >
+                    faq
+                  </NavLink>
+                </li>
+              </ul>
             </div>
-          </li>
+          </div>
         </ul>
 
         {/* Hamburger */}
@@ -193,92 +192,209 @@ const Navbar = () => {
           isOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 ease-in-out z-50`}
       >
-        <button
-          className="absolute top-4 right-4 text-gray-800 focus:outline-none"
-          onClick={() => setIsOpen(false)}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth={2}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-        <ul className="flex flex-col space-y-4 mt-16 px-6 text-gray-600 font-medium">
-          <li>
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? activeClassName
-                  : "hover:text-primary transition duration-200"
-              }
+        {!isSubMenuAboutUsOpen && !isSubMenuOurTeamOpen ? (
+          <div>
+            {/* Main Menu */}
+            <button
+              className="absolute top-4 right-4 text-gray-800 focus:outline-none"
               onClick={closeMenu}
             >
-              home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/services"
-              className={({ isActive }) =>
-                isActive
-                  ? activeClassName
-                  : "hover:text-primary transition duration-200"
-              }
-              onClick={closeMenu}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+            <ul className="flex flex-col space-y-4 mt-16 px-6 text-gray-600 font-medium">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? activeClassName
+                      : "hover:text-primary transition duration-200"
+                  }
+                  onClick={closeMenu}
+                >
+                  home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/services"
+                  className={({ isActive }) =>
+                    isActive
+                      ? activeClassName
+                      : "hover:text-primary transition duration-200"
+                  }
+                  onClick={closeMenu}
+                >
+                  services
+                </NavLink>
+              </li>
+              <li>
+                <button
+                  className="text-left w-full text-gray-600 hover:text-primary transition duration-200"
+                  onClick={() => setIsSubMenuOurTeamOpen(true)}
+                >
+                  our team
+                </button>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact-us"
+                  className={({ isActive }) =>
+                    isActive
+                      ? activeClassName
+                      : "hover:text-primary transition duration-200"
+                  }
+                  onClick={closeMenu}
+                >
+                  contact us
+                </NavLink>
+              </li>
+              <li>
+                <button
+                  className="text-left w-full text-gray-600 hover:text-primary transition duration-200"
+                  onClick={() => setIsSubMenuAboutUsOpen(true)}
+                >
+                  about us
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : isSubMenuAboutUsOpen ? (
+          <div>
+            {/* Sub Menu About Us */}
+            <button
+              className="absolute top-4 left-4 text-gray-800 focus:outline-none"
+              onClick={() => setIsSubMenuAboutUsOpen(false)}
             >
-              services
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/our-team"
-              className={({ isActive }) =>
-                isActive
-                  ? activeClassName
-                  : "hover:text-primary transition duration-200"
-              }
-              onClick={closeMenu}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </button>
+            <ul className="flex flex-col space-y-4 mt-16 px-6 text-gray-600 font-medium">
+              <li>
+                <NavLink
+                  to="/office"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  office tour
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/reviews"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  reviews
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/safety"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  safety
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/faq"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  faq
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div>
+            {/* Sub Menu Our Team */}
+            <button
+              className="absolute top-4 left-4 text-gray-800 focus:outline-none"
+              onClick={() => setIsSubMenuOurTeamOpen(false)}
             >
-              our team
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/contact-us"
-              className={({ isActive }) =>
-                isActive
-                  ? activeClassName
-                  : "hover:text-primary transition duration-200"
-              }
-              onClick={closeMenu}
-            >
-              contact us
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/about"
-              className={({ isActive }) =>
-                isActive
-                  ? activeClassName
-                  : "hover:text-primary transition duration-200"
-              }
-              onClick={closeMenu}
-            >
-              about us
-            </NavLink>
-          </li>
-        </ul>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15 19l-7-7 7-7"
+                />
+              </svg>
+            </button>
+            <ul className="flex flex-col space-y-4 mt-16 px-6 text-gray-600 font-medium">
+              <li>
+                <NavLink
+                  to="/our-team"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  overview
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dentist-dr-john-doe"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  dr. john doe
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dentist-dr-jane-smith"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  dr. jane smith
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/dentist-dr-alice-johnson"
+                  className="hover:text-primary transition duration-200"
+                  onClick={closeMenu}
+                >
+                  dr. alice johnson
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
